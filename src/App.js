a18c851852964eb02328ae3b6b8b3e9ed1dac819
Vadim Losenkov <vprog0729@gmail.com>
2021-09-19 17:43:08 +0300

    "changes"

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Card from './components/Card/index.js';
+// import Card from './components/Card/index.js';
 import Drawer from './components/Drawer.js';
 import Header from './components/Header.js';
 
 import axios from 'axios'
 
 function App() {
-  const [sneakers, setSneakers] = React.useState([])
+  // const [sneakers, setSneakers] = React.useState([])
   const [cartItems, setCartItems] = React.useState([])
   const [cartOpened, setCartOpened] = React.useState(false)
   
@@ -14,9 +14,9 @@ function App() {
     setCartOpened(!cartOpened)
   }
   
-  const onAddToCart = (obj) => {
+  /* const onAddToCart = (obj) => {
     setCartItems([...cartItems, obj])
-  }
+  } */
   
   React.useEffect(() => {
     /*
@@ -30,7 +30,7 @@ function App() {
   
   return (
     <div className="wrapper clear">
-      {cartOpened && <Drawer onCart={onCartClick} items={cartItems} />}
+      {/* cartOpened && <Drawer onCart={onCartClick} items={cartItems} /> */}
       <Header onCart={onCartClick} />
       <div className="content p-40">
         <div className="d-flex justify-between align-center mb-40">
